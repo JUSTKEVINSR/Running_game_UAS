55cc9a97eeb6ad8090dd56776492f5fdf7480103
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-07-14 12:23:44 +0700

    modification #1 remove the CharacterMoveControl Script
    all code is remove for modification

--- run man uts/Assets/scrip/CharacterMoveController.cs
+++ run man uts/Assets/scrip/CharacterMoveController.cs
@@ -4,114 +4,30 @@ using UnityEngine;
 
 public class CharacterMoveController : MonoBehaviour
 {
-    [Header("Movement")]
-    public float moveAccel;
-    public float maxSpeed;
-    private Rigidbody2D rig;
 
-    [Header("Jump")]
-    public float jumpAccel;
 
-    private bool isJumping;
-    private bool isOnGround;
-    public float jumpAmount = 10;
-
-    [Header("Ground Raycast")]
-    public float groundRaycastDistance;
-    public LayerMask groundLayerMask;
-
-    private Animator anim;
-    private CharacterSoundController sound;
-
-    [Header("Scoring")]
-    public ScoreController score;
-    public float scoringRatio;
-    private float lastPositionX;
-
-    [Header("GameOver")]
-    public float fallPositionY;
-    public GameObject gameOverScreen;
-
-    [Header("Camera")]
-    public CameraMoveController gameCamera;
-    // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sound = GetComponent<CharacterSoundController>();
-    }
 
-    void FixedUpdate()
-    {
-        // raycast ground
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
-        groundRaycastDistance, groundLayerMask);
-        if (hit)
-        {
-            if (!isOnGround && rig.velocity.y <= 0)
-            {
-                isOnGround = true;
-            }
-        }
-        else
-        {
-            isOnGround = false;
-        }
-        // calculate velocity vector
-        Vector2 velocityVector = rig.velocity;
-        if (isJumping)
-        {
-            velocityVector.y += jumpAccel;
-            isJumping = false;
-        }
-        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
-        0.0f, maxSpeed);
-        rig.velocity = velocityVector;
     }
 
     private void OnDrawGizmos()
     {
-        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
-        groundRaycastDistance), Color.white);
+
     }
 
     private void GameOver()
     {
-        // set high score
-        score.FinishScoring();
-        // stop camera movement
-        gameCamera.enabled = false;
-        // show gameover
-        gameOverScreen.SetActive(true);
-        // disable this too
-        this.enabled = false;
+
     }
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        // read input
-        if (Input.GetMouseButtonDown(0))
-        {
-            
-            {
-                rig.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
-                sound.PlayJump();
-            }
-        }
-        // change animation
-        anim.SetBool("isOnGround", isOnGround);
+
+    }
+
+    private void FixedUpdate()
+    {
         
-        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
-        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
-        if (scoreIncrement > 0)
-        {
-            score.IncreaseCurrentScore(scoreIncrement);
-            lastPositionX += distancePassed;
-        }
-        if (transform.position.y < fallPositionY)
-        {
-            GameOver();
-        }
     }
 }
