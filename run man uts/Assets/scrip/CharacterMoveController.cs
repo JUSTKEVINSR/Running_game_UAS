2de2b969a9b849e76776b7409cb01bd3b624012d
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-07-15 10:15:19 +0700

    modification #2 add move front,back and jump
    tambah lompat dam maju mundur

--- run man uts/Assets/scrip/CharacterMoveController.cs
+++ run man uts/Assets/scrip/CharacterMoveController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CharacterMoveController : MonoBehaviour
 {
+    private Rigidbody2D rb2D;
+
+    [Header("basic Movement")]
+    public float moveSpeed;
+    public float jumpforse;
+    private bool isjumping;
+    private float moveHorizontal;
+    private float moveVertical;
+
 
 
     void Start()
     {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
 
+        
     }
 
     private void OnDrawGizmos()
@@ -23,11 +34,24 @@ public class CharacterMoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            {
+                rb2D.AddForce(Vector2.up * jumpforse, ForceMode2D.Impulse);
+                
+            }
+        }
     }
 }
