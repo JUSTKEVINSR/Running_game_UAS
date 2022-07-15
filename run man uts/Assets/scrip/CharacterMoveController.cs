e0ad4454cfbb0e74afaf5a32b5d284f334a97794
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-07-15 14:14:16 +0700

    modification #3 putting every code back
    putting every code back (only for CharacterMoveControler)

--- run man uts/Assets/scrip/CharacterMoveController.cs
+++ run man uts/Assets/scrip/CharacterMoveController.cs
@@ -9,33 +9,79 @@ public class CharacterMoveController : MonoBehaviour
     [Header("basic Movement")]
     public float moveSpeed;
     public float jumpforse;
+    public float maxSpeed;
+    public float jumpAmount;
+
     private bool isjumping;
     private float moveHorizontal;
     private float moveVertical;
 
+    private bool isOnGround;
 
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("GameOver")]
+    public float fallPositionY;
+    public GameObject gameOverScreen;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    private Animator anim;
+    private CharacterSoundController sound;
 
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
 
-        
     }
 
     private void OnDrawGizmos()
     {
-
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
+        groundRaycastDistance), Color.white);
     }
 
     private void GameOver()
     {
-
+        // set high score
+        score.FinishScoring();
+        // stop camera movement
+        gameCamera.enabled = false;
+        // show gameover
+        gameOverScreen.SetActive(true);
+        // disable this too
+        this.enabled = false;
     }
     // Update is called once per frame
     void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
+
+        // change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+        if (transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
     }
 
     private void FixedUpdate()
@@ -50,8 +96,31 @@ public class CharacterMoveController : MonoBehaviour
 
             {
                 rb2D.AddForce(Vector2.up * jumpforse, ForceMode2D.Impulse);
-                
+                sound.PlayJump();
             }
         }
+
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+        groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rb2D.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        // calculate velocity vector
+        Vector2 velocityVector = rb2D.velocity;
+        if (isjumping)
+        {
+            velocityVector.y += jumpforse;
+            isjumping = false;
+        }
+
     }
 }
